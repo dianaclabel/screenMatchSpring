40cd76ab88dd4a502a4cbfbd1f1540d361588276
diana <dianaclabel08@gmail.com>
2025-01-16 23:59:20 -0500

    Utilizando streams, manipulación de fechas y funciones lambda

--- src/main/java/com/aluracursos/screenMatchSpring/principal/Principal.java
+++ src/main/java/com/aluracursos/screenMatchSpring/principal/Principal.java
@@ -10,10 +10,7 @@ import org.springframework.cglib.core.Local;
 
 import java.time.LocalDate;
 import java.time.format.DateTimeFormatter;
-import java.util.ArrayList;
-import java.util.Comparator;
-import java.util.List;
-import java.util.Scanner;
+import java.util.*;
 import java.util.stream.Collectors;
 
 public class Principal {
@@ -75,33 +72,70 @@ public class Principal {
         // filter nos permite hacer un filtro que excluyan los N/A
         //equalsIgnoreCase nos permite hacer una comparacion sin tener en en cuenta palabras mayusculas o minusculas devuelve un boolean
         //sorted ordena de menor a mayor, comparator.comparing permite por evalucion, reversed nos traerá los datos de menor a mayor
-        System.out.println("Top 5 episodios");
+        /*System.out.println("Top 5 episodios");
         datosEpisodios.stream()
                 .filter(e -> !e.evaluacion().equalsIgnoreCase("N/A") )
+                .peek(e -> System.out.println("Primer filtro (N/A)" + e ))
                 .sorted(Comparator.comparing(DatosEpisodio::evaluacion).reversed())
+                .peek(e -> System.out.println("Segundo filtro (M>m)" + e ))
+                .map(e -> e.titulo().toUpperCase())
+                .peek(e -> System.out.println("Tercer filtro Mayuscula (M>m)" + e))
                 .limit(5)
                 .forEach(System.out::println);
+
+         */
+
+
 
         //convirtiendo los Datos a una lista de tipo Episodio.
         List<Episodio> episodios = temporadas.stream()
                 .flatMap(t -> t.episodios().stream().map(d -> new Episodio(t.numero(),d))).collect(Collectors.toList());
-        episodios.forEach(System.out::println);
+        //episodios.forEach(System.out::println);
 
         // Busqueda de episodios a partir de X año
+        /*
         System.out.println("Por favor indica el año del cual deseas ver los episodios");
 
         var fecha = teclado.nextInt();
         teclado.nextLine();
         LocalDate fechaDeBusqueda = LocalDate.of(fecha, 1 , 1);
 
-        DateTimeFormatter dtf = DateTimeFormatter.ofPattern("dd/MM/yyyy");
-
+        DateTimeFormatter dtf = DateTimeFormatter.ofPattern("dd/MM/yyyy");+/
+/*
         episodios.stream().filter(e -> e.getFechaDeLanzamiento() != null && e.getFechaDeLanzamiento().isAfter(fechaDeBusqueda))
                 .forEach( e -> System.out.println(
                         "Temporada "+ e.getTemporada() +
                                 "Episodio "+ e.getTitulo() +
                                 " Fecha de Lanzamiento " + e.getFechaDeLanzamiento().format(dtf)
                 ));
+
+*/
+
+        // Busca episodios por un pedazo del titulo
+        //findFirst va a encontrar la primera coincidencia
+
+        /*
+        System.out.println("Por favor escriba el titulo del episodio que desea ver");
+        var pedazoTitulo = teclado.nextLine();
+        Optional<Episodio> episodioBuscado = episodios.stream()
+                .filter(e -> e.getTitulo().toUpperCase().contains(pedazoTitulo.toUpperCase()))
+                .findFirst();
+
+        if (episodioBuscado.isPresent()){
+            System.out.println("Episodio buscado");
+            System.out.println("los datos son: " + episodioBuscado.get());
+        }else {
+            System.out.println("Episodio no encontrado ");
+        }
+         */
+
+        Map<Integer,Double> evaluacionesPorTemporada = episodios.stream()
+                .filter(e -> e.getEvaluacion() > 0.0)
+                .collect(Collectors.groupingBy(Episodio::getTemporada,
+                        Collectors.averagingDouble(Episodio::getEvaluacion) ));
+
+        System.out.println(evaluacionesPorTemporada);
+
 
 
 
